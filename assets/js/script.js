72ddf06911781b78e9c30feb47d744511eded351
Ashfaque Hossain Abir <108189537+Abirett3@users.noreply.github.com>
2023-05-28 11:40:30 +0600

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,3 +70,41 @@ for (let i = 0, len = revealDelayElements.length; i < len; i++) {
 
 window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
+
+/**
+ * Contact Form Script
+ */
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxDJF2M4PpjjtTCkSn0CO6jnmsMNQw3xojDtEEk9MtYiNoQ9X9dlHvaOq05CDz_tM8/exec'
+
+    const form = document.forms['submit-to-google-sheet']
+
+    const msg = document.getElementById('msg')
+
+  
+
+    form.addEventListener('submit', e => {
+
+      e.preventDefault()
+
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+
+        .then(response => {
+
+          msg.innerHTML = "Message Sent!!"
+
+        })
+
+        setTimeout(function() {
+
+          msg.innerHTML = ""
+
+  
+
+        }, 10000)
+
+        form.reset()
+
+        .catch(error => console.error('Error!', error.message))
+
+    })
