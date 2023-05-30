c20823780e8bb545ee8db966708e09602f9d0d9c
Ashfaque Hossain Abir <108189537+Abirett3@users.noreply.github.com>
2023-05-30 23:16:32 +0545

    Updated

--- script2.js
+++ script2.js
@@ -39,10 +39,14 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbxDJF2M4PpjjtTCkSn0CO
 
 //-------------- For prevent hacking -----------------
 
+/*----| Deactivated |----
+
 // Disable context menu
 document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
  }, false);
+
+----*/
 
  // Disable “Ctrl+Shift+I”, “Ctrl+U” and ”F12 key
  document.addEventListener("keydown", (e) => {
@@ -51,3 +55,36 @@ document.addEventListener("contextmenu", (e) => {
    e.preventDefault();
   }
  });
+
+ 
+ /**
+ * Custom Context Menu
+ */
+
+const contextMenu = document.querySelector(".wrapper"),
+shareMenu = contextMenu.querySelector(".share-menu");
+
+window.addEventListener("contextmenu", e => {
+    e.preventDefault();
+    let x = e.offsetX, y = e.offsetY,
+    winWidth = window.innerWidth,
+    winHeight = window.innerHeight,
+    cmWidth = contextMenu.offsetWidth,
+    cmHeight = contextMenu.offsetHeight;
+
+    if(x > (winWidth - cmWidth - shareMenu.offsetWidth)) {
+        shareMenu.style.left = "-200px";
+    } else {
+        shareMenu.style.left = "";
+        shareMenu.style.right = "-200px";
+    }
+
+    x = x > winWidth - cmWidth ? winWidth - cmWidth - 5 : x;
+    y = y > winHeight - cmHeight ? winHeight - cmHeight - 5 : y;
+    
+    contextMenu.style.left = `${x}px`;
+    contextMenu.style.top = `${y}px`;
+    contextMenu.style.visibility = "visible";
+});
+
+document.addEventListener("click", () => contextMenu.style.visibility = "hidden");
